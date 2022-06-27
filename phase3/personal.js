6754e83951b3c33f5f253b3a1e6d4454cd253dfd
LiatHanunu <liat.hanunu01@gmail.com>
2022-06-27 15:39:22 +0300

    phase 3

--- phase3/personal.js
+++ phase3/personal.js
@@ -1,12 +1,45 @@
 
-async function getHobbies() {
-    const response = await fetch('https://github.com/yossipoli/TheWizard/blob/main/hobbies.json', { mode: 'no-cors'});
-    const hobbies = await response.json();
-    console.log(hobbies);
-}
-getHobbies()
+/*async function getHobbies() {
+    const response = await fetch("hobbies.json",{
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        })
+        if (response.ok) {
+            const jsonValue = await response.json(); // Get JSON value from the response body
+            return Promise.resolve(jsonValue);
+          } else {
+            return Promise.reject('*** PHP file not found');
+          }
+        }
+    //const hobbies= await res.json()
+    // .then(hobbies=>createHobbies(hobbies))
+    //createHobbies(hobbies)
+ */   
 
+const hobbies = ["cooking",
+"baking",
+"singning",
+"dancing",
+"reading",
+"football",
+"basketball",
+"tennis",
+"running",
+"baseball",
+"driving",
+"gaming",
+"playing piano",
+"camping",
+"fishing",
+"hunting",
+"studying",
+"cliff climbing",
+"diveing",
+"horse riding",
+"watch tv",
+"movies"]
 
+const data = JSON.parse(localStorage.getItem("wizardUser"));
 
 function createHobbies(hobbies){
     const hobbiesPlace = document.getElementById("hobbies")
@@ -21,4 +54,16 @@ function createHobbies(hobbies){
         hobbiesPlace.append(hobbieElement, hobbieLabel)
     }
 }
+createHobbies(hobbies)
+
+const photoPlace = document.getElementById("userPhoto")
+photoPlace.addEventListener('input',()=>{
+    data.img = photoPlace.value
+    const photo = document.createElement("img")
+    photo.src = photoPlace.value 
+    const photoPart = document.getElementById("photo")
+    photoPart.append(photo)
+
+}
+)
 //createHobbies(hobbies)
